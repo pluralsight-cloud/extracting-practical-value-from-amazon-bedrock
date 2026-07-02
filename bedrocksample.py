f6bef728e7d276186f0d07647dd01c78b91c47dc
Stefan Nychka <stefan-nychka@pluralsight.com>
2026-07-02 11:13:52 -0600

    Create bedrocksample.py
    adding in file referenced in last vid (lab Establishing a Bounded Amazon Bedrock Invocation Pattern)

---
+++ bedrocksample.py
@@ -0,0 +1,22 @@
+import boto3
+
+client = boto3.client("bedrock-runtime")
+
+response = client.converse(
+    modelId="amazon.nova-pro-v1:0",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "text": "Explain cloud computing in three bullet points."
+                }
+            ]
+        }
+    ],
+    inferenceConfig={
+        "maxTokens": 300,
+        "temperature": 0.2,
+        "topP": 0.9
+    }
+)
